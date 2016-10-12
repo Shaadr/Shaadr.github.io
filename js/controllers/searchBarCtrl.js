0f3de66e65ada72327ac6a3c369dd6697db7b7b4
Michael Hanks <mikeHanks@Michaels-MBP.home>
2016-10-11 19:22:20 -0600

    api fully functioning. project finished

--- js/controllers/searchBarCtrl.js
+++ js/controllers/searchBarCtrl.js
@@ -5,38 +5,13 @@
   .controller('searchBarCtrl', function($scope, mainServ) {
 
 
-      $scope.vids = function(){
-        mainServ.getClips().then(function(response) {
+      $scope.vids = function(searchText){
+        mainServ.getClips(searchText).then(function(response) {
           console.log(response);
-          $scope.movies = response.items;
+          $scope.movies = response;
         })
-        console.log($scope.videos)
       }
 
-
-  $scope.getOutput = function(item){
-  	$scope.title = item.snippet.title;
-  	$scope.description = item.snippet.description;
-  	$scope.thumb = item.snippet.thumbnails.high.url;
-  	$scope.channelTitle = item.snippet.channelTitle;
-  	$scope.videoDate = item.snippet.publishedAt;
-
-	// Build Output String
-  	var output = '<li>' +
-  	'<div class="list-left">' +
-  	'<img src="'+thumb+'">' +
-  	'</div>' +
-  	'<div class="list-right">' +
-  	'<h3><a href="https://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>' +
-  	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
-  	'<p>'+description+'</p>' +
-  	'</div>' +
-  	'</li>' +
-  	'<div class="clearfix"></div>' +
-  	'';
-
-	return output;
-}
 
 
 })
--- js/mainService.js
+++ js/mainService.js
@@ -2,18 +2,18 @@ angular.module('gooTube')
 .service('mainServ', function($http, $q){
 
 
-// this.search = function() {
-  this.q = $('#query').val();
-  this.getClips = function(){
+  this.getClips = function(searchText){
+    console.log(searchText);
+
     return $http({
       method: "GET",
-      url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q=javascript&type=video&key=AIzaSyAnq1Bc7Tj9uknrfNaOUGiu4O-JaYwDnKM"
+      url: "https://www.googleapis.com/youtube/v3/search/?q=" + searchText + "&key=AIzaSyAnq1Bc7Tj9uknrfNaOUGiu4O-JaYwDnKM&part=snippet&maxResults=8&type=video"
+      // part=snippet&q=javascript&type=video&key=AIzaSyAnq1Bc7Tj9uknrfNaOUGiu4O-JaYwDnKM"
   			// key: 'AIzaSyAnq1Bc7Tj9uknrfNaOUGiu4O-JaYwDnKM',
     }).then(function(response){
-      // console.log(response.data)
-      return response.data;
+      // console.log(response)
+      return response.data.items;
     })
   }
-// }
 
 })
